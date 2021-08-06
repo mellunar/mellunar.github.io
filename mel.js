dab3280d37a2d2b90f5eb9fcf5a5dbd071ee25f9
Melissa Fernandes <87154023+mellunar@users.noreply.github.com>
2021-08-06 20:13:24 -0300

    Update mel.js

--- mel.js
+++ mel.js
@@ -1,4 +1,6 @@
-window.addEventListener("load", function(){
+document.addEventListener("DOMContentLoaded", function() {
+    let cpg = document.location.href.substring(document.location.href.lastIndexOf("/")+1, document.location.href.length);
+    //cpg == "index.html" ? alert("OK") : void(0);
     window.mobilemenu = function mobilemenu() {
       let o = document.getElementById("moblinks");
       o.style.display === "flex" ? o.style.display = "none" : o.style.display = "flex"
@@ -15,7 +17,62 @@ window.addEventListener("load", function(){
       document.getElementById("header").classList.toggle("darkheader");
       document.getElementsByClassName("tbbg")[0].classList.toggle("darktbbg");
       document.getElementById("footer").classList.toggle("darkfooter");
-      var welcome = document.getElementById("welcome")
+      let welcome = document.getElementById("welcome")
       welcome != null ? welcome.classList.toggle("darkwelcome") : void(0);
     }
+
+    //nav selected
+    if (cpg == "biografia.html") {
+      let libio = document.getElementById("li-bio")
+      libio.innerHTML = "Biografia";
+      libio.classList.toggle("green")
+    }
+    if (cpg == "portfolio.html") {
+      let liptf = document.getElementById("li-ptf")
+      liptf.innerHTML = "Portfolio";
+      liptf.classList.toggle("green")
+    }
+    else {
+      void(0);
+    }
 });
+
+class Topbar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <nav id="topbar" class="tbbg">
+    <div id="tbc">
+        <div id="logo"><a href="https://mellunar.github.io/">[Melissa Fernandes]</a></div>
+        <div id="mobmenu"><a href="javascript:void(0)" onclick="mobilemenu()" role="button"><i class="bi bi-list" onclick="openClose(this)"></i></a></div>
+        <div id="topnav">
+            <ul id="moblinks">
+                <li id="li-ptf"><a href="https://mellunar.github.io/portfolio">Portfolio</a></li>
+                <li id="li-bio"><a href="https://mellunar.github.io/biografia">Biografia</a></li>
+            </ul>
+        </div>
+        <button id="daynight" onclick="dayNight(); toggleDark()"><i class="bi bi-moon-fill"></i></button>
+    </div>        
+</nav>
+    `
+  }
+}
+
+class Footer extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = `
+    <footer id="footer">
+      <div id="ifooter">
+          <div id="mel"><span id="flogo">[Melissa Fernandes]</span></div>
+          <nav id="footer-links">
+              <a href="mailto:melissafernandes93@yahoo.com.br"><i class="bi bi-at"></i></a>
+              <a href="https://github.com/mellunar"><i class="bi bi-github"></i></a>
+              <a href="https://www.linkedin.com/in/melissa-fernandes-610033163/"><i class="bi bi-linkedin"></i></a>
+          </nav>
+      </div>
+    </footer>
+    `
+  }
+}
+
+customElements.define('top-bar', Topbar);
+customElements.define('pg-footer', Footer);
