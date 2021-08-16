bdb29f95a50379fe451bd8ba03378725b539417c
mellunar <melfer100@gmail.com>
2021-08-15 22:04:25 -0300

    atualização versão 3

--- mel.js
+++ mel.js
@@ -1,80 +1,119 @@
-document.addEventListener("DOMContentLoaded", function() {
-    window.mobilemenu = function mobilemenu() {
-      let o = document.getElementById("moblinks");
-      o.style.display === "flex" ? o.style.display = "none" : o.style.display = "flex"
-    }
-    window.openClose = function openClose(x) {
-      x.classList.toggle("bi-x");
-    }
-    window.dayNight = function dayNight() {
-      let d = document.getElementById("daynight");
-      d.innerHTML === `<i class="bi bi-moon-fill"></i>` ? d.innerHTML = `<i class="bi bi-sun-fill"></i>` : d.innerHTML = `<i class="bi bi-moon-fill"></i>`
-    }
-    window.toggleDark = function toggleDark() {
-      document.body.classList.toggle("darkbody");
-      document.getElementById("header").classList.toggle("darkheader");
-      document.getElementsByClassName("tbbg")[0].classList.toggle("darktbbg");
-      document.getElementById("footer").classList.toggle("darkfooter");
-      const welcome = document.getElementById("welcome")
-      welcome != null ? welcome.classList.toggle("darkwelcome") : void(0);
-      const portfolio = document.getElementById("portfolio");
-      portfolio != null ? portfolio.classList.toggle("darkportsec") : void(0);
-    }
+const dmmoon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16">
+<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+</svg>
+`;
+const dmsun = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16">
+<path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+</svg>
+`;
+const mbhamb = `
+<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>`
+;
+const mbx = `
+<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+`;
+const html = document.querySelector("html");
+const currentTheme = localStorage.getItem('theme');
+currentTheme == null ? void(0) : void(0);
 
-    //nav selected
-    let pgptf = document.getElementById("portfolio");
-    let pgbio = document.getElementById("biografia");
-    if (pgbio != null) {
-      let libio = document.getElementById("li-bio")
-      libio.innerHTML = "Biografia";
-      libio.classList.toggle("green")
-    }
-    if (pgptf != null) {
-      let liptf = document.getElementById("li-ptf")
-      liptf.innerHTML = "Portfolio";
-      liptf.classList.toggle("green")
-    }
-    else {
-      void(0);
+customElements.define('top-bar', class Topbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <nav id="topbar">
+            <div id="tbc">
+                <button id="mobmenu" onclick="touchMenu(this)" data-mbmenu="mbhamb" hidden>${mbhamb}</button>
+                <div id="logo"><a href="https://mellunar.github.io/">[Melissa Fernandes]</a></div>
+                <div id="topnav">
+                    <ul id="moblinks">
+                        <li id="li-ptf"><a href="https://mellunar.github.io/portfolio">Portfolio</a></li>
+                        <li id="li-gist"><a href="https://mellunar.github.io/gists">Gists</a></li>
+                        <li id="li-bio"><a href="https://mellunar.github.io/biografia">Biografia</a></li>
+                    </ul>
+                </div>            
+                <button id="dmswitcher" onclick="darkMode(this)" aria-hidden="true">${dmmoon}</button>
+            </div>        
+        </nav>
+        `
     }
 });
 
-class Topbar extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-    <nav id="topbar" class="tbbg">
-    <div id="tbc">
-        <div id="logo"><a href="https://mellunar.github.io/">[Melissa Fernandes]</a></div>
-        <div id="mobmenu"><a href="javascript:void(0)" onclick="mobilemenu()" role="button"><i class="bi bi-list" onclick="openClose(this)"></i></a></div>
-        <div id="topnav">
-            <ul id="moblinks">
-                <li id="li-ptf"><a href="https://mellunar.github.io/portfolio">Portfolio</a></li>
-                <li id="li-bio"><a href="https://mellunar.github.io/biografia">Biografia</a></li>
-            </ul>
+document.addEventListener("DOMContentLoaded", function() {
+    const dmswitcher = document.getElementById("dmswitcher");
+    const mobmenu = document.getElementById("topnav");
+    const pgptf = document.getElementById("portfolio");
+    const pgist = document.getElementById("gists");
+    const pgbio = document.getElementById("biografia");
+    const liptf = document.getElementById("li-ptf")
+    const ligist = document.getElementById("li-gist")
+    const libio = document.getElementById("li-bio")
+
+    window.darkMode = function darkMode(x){
+        if (html.dataset.theme == "light"){
+            x.innerHTML = dmsun;
+            html.setAttribute("data-theme", "dark");
+            localStorage.setItem("theme", "dark");
+        }
+        else {
+            x.innerHTML = dmmoon;
+            html.setAttribute("data-theme", "light");
+            localStorage.setItem("theme", "light");
+        }
+    }
+    
+    if (currentTheme === "dark" || window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        dmswitcher.innerHTML = dmsun;
+        html.setAttribute("data-theme", "dark");
+    }
+
+    if (pgptf != null) {
+        liptf.innerHTML = "Portfolio";
+        liptf.classList.toggle("green")
+    }
+    if (pgist != null) {
+        ligist.innerHTML = "Gists";
+        ligist.classList.toggle("green")
+    }
+    if (pgbio != null) {
+        libio.innerHTML = "Biografia";
+        libio.classList.toggle("green")
+    }
+    else {
+        void(0);
+    }
+    
+    window.touchMenu = function touchMenu(x) {
+        if (x.dataset.mbmenu == "mbhamb") {
+            x.innerHTML = mbx;
+            x.setAttribute("data-mbmenu", "mbx");
+            mobmenu.style.display = "block";
+        }
+        else {
+            x.innerHTML = mbhamb;
+            x.setAttribute("data-mbmenu", "mbhamb");
+            mobmenu.style.display = "none";
+        }
+    }
+});
+
+customElements.define('pg-footer', class Footer extends HTMLElement {
+    connectedCallback(){
+      this.innerHTML = `
+      <footer id="footer">
+        <div id="ifooter">
+            <div>[Melissa Fernandes]</div>
+            <nav id="footer-links">
+                <a href="mailto:melissafernandes93@yahoo.com.br"><i class="bi bi-at"></i></a>
+                <a href="https://github.com/mellunar"><i class="bi bi-github"></i></a>
+                <a href="https://www.linkedin.com/in/melissa-fernandes-610033163/"><i class="bi bi-linkedin"></i></a>
+            </nav>
         </div>
-        <button id="daynight" onclick="dayNight(); toggleDark()"><i class="bi bi-moon-fill"></i></button>
-    </div>        
-</nav>
-    `
-  }
-}
-
-class Footer extends HTMLElement {
-  connectedCallback(){
-    this.innerHTML = `
-    <footer id="footer">
-      <div id="ifooter">
-          <div id="mel"><span id="flogo">[Melissa Fernandes]</span></div>
-          <nav id="footer-links">
-              <a href="mailto:melissafernandes93@yahoo.com.br"><i class="bi bi-at"></i></a>
-              <a href="https://github.com/mellunar"><i class="bi bi-github"></i></a>
-              <a href="https://www.linkedin.com/in/melissa-fernandes-610033163/"><i class="bi bi-linkedin"></i></a>
-          </nav>
-      </div>
-    </footer>
-    `
-  }
-}
-
-customElements.define('top-bar', Topbar);
-customElements.define('pg-footer', Footer);
+      </footer>
+      `
+    }
+});
